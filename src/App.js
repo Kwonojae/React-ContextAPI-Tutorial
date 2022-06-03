ec2a9d208e8fcf4bf21375cb1bcb60fb3aa8285b
Ojea <rnjsdhwo1325@gmail.com>
2022-06-03 21:43:01 +0900

    contextAPi CartADD Basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { CartProvider } from "./CartContext";
+import Checkout from "./components/Checkout";
+import Nav from "./components/Nav";
+import Products from "./components/Products";
 
 function App() {
-  return <div className="App">하이</div>;
+  return (
+    <div className="App">
+      <CartProvider>
+        <BrowserRouter>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { AiFillShopping } from "react-icons/ai";
+import { IoShirtOutline } from "react-icons/io5";
+import { useContext } from "react";
+import CartContext from "../CartContext";
+
+function Card({ name, price }) {
+  const { addToCart } = useContext(CartContext);
+  return (
+    <div onClick={() => addToCart(name, price)} className="card">
+      <div className="product-box">
+        <IoShirtOutline />
+      </div>
+      <div className="purchase">
+        <h3>{name}</h3>
+        <AiFillShopping />
+      </div>
+      <h4>${price}</h4>
+    </div>
+  );
+}
+
+export default Card;
